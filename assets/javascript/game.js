252428cc37334d591bb720012fab514f30182853
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-06-20 15:13:37 -0400

    Tue 3:13

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,20 +3,26 @@
 	var wins = 0;
 	var losses = 0;
 	var colorsArray = ["red", "blue", "green"];
-	var guesses = 8;
+	var guesses = 9;
 	var currentWord = "";
+	var displayWord = "";
+	var letterBank = [];
+	var letterPrint = letterBank.join();
+	var selectedLetter = "";
 
 	// Red Variables]
 		var red0 = "_ ";
 		var red1 = "_ ";
 		var red2 = "_";
 		var redWord = red0 + red1 + red2;
+		var redIndex = colorsArray.indexOf("red");
 	// Blue Variables
 		var blue0 = "_ ";
 		var blue1 = "_ ";
 		var blue2 = "_ ";
 		var blue3 = "_";
 		var blueWord = blue0 + blue1 + blue2 + blue3;
+		var blueIndex = colorsArray.indexOf("blue");
 	//Green Variables
 		var green0 = "_ ";
 		var green1 = "_ ";
@@ -24,6 +30,7 @@
 		var green3 = "_ ";
 		var green4 = "_";
 		var greenWord = green0 + green1 + green2 + green3 + green4;
+		var greenIndex = colorsArray.indexOf("green");
 
 	// console.log(wins);
 	// console.log(losses);
@@ -36,8 +43,14 @@
 
 // Functions
 	function chooseWord () {
-		var currentWord = colorsArray[Math.floor(Math.random () * colorsArray.length)];
+		currentWord = colorsArray[Math.floor(Math.random () * colorsArray.length)];
 		console.log(currentWord);
+		guesses = 9;
+	}
+
+	function letterCache () {
+		selectedLetter = String.fromCharCode(event.keyCode).toLowerCase();
+		letterBank.push(selectedLetter);
 	}
 
 // Running Code
@@ -49,16 +62,132 @@
 		console.log(userGuess);
 
 		if (currentWord == "red") {
+			displayWord = redWord;
 			if (userGuess == "r") {
-				var red0 = "r";
+				red0 = "r";
+				redWord = red0 + red1 + red2;
 			}
 			else if (userGuess == "e") {
-				var red1 = "e";
+				red1 = "e";
+				redWord = red0 + red1 + red2;
 			}
 			else if (userGuess == "d") {
-				var red2 = "d";
+				red2 = "d";
+				redWord = red0 + red1 + red2;
+			}
+			else {
+				guesses--;
+				letterCache();
+				if (guesses < 0) {
+					losses++;
+					if (redIndex > -1) {
+						colorsArray.splice(redIndex, 1);
+					} 
+					chooseWord();
+				}
+			}
+			if (red0 == "r" && red1 == "e" && red2 == "d") {
+				wins++;
+				if (redIndex > -1) {
+						colorsArray.splice(redIndex, 1);
+					} 
+				chooseWord();
 			}
 			console.log(redWord);
+			console.log(guesses);
 		}
+
+		if (currentWord == "blue") {
+			displayWord = blueWord;
+			if (userGuess == "b") {
+				blue0 = "b";
+				blueWord = blue0 + blue1 + blue2 + blue3;
+			}
+			else if (userGuess == "l") {
+				blue1 = "l";
+				blueWord = blue0 + blue1 + blue2 + blue3;
+			}
+			else if (userGuess == "u") {
+				blue2 = "u";
+				blueWord = blue0 + blue1 + blue2 + blue3;
+			}
+			else if (userGuess == "e") {
+				blue3 = "e";
+				blueWord = blue0 + blue1 + blue2 + blue3;
+			}
+			else {
+				guesses--;
+				letterCache();
+				if (guesses < 0) {
+					losses++;
+					if (blueIndex > -1) {
+						colorsArray.splice(blueIndex, 1);
+					} 
+					chooseWord();
+				}
+			}
+			if (blue0 == "b" && blue1 == "l" && blue2 == "u" && blue3 == "e") {
+				wins++;
+				if (blueIndex > -1) {
+						colorsArray.splice(blueIndex, 1);
+				} 
+				chooseWord();
+			}
+			console.log(blueWord);
+			console.log(guesses);
+		}
+
+		if (currentWord == "green") {
+			displayWord = greenWord;
+			if (userGuess == "g") {
+				green0 = "g";
+				greenWord = green0 + green1 + green2 + green3 + green4;
+			}
+			else if (userGuess == "r") {
+				green1 = "r";
+				greenWord = green0 + green1 + green2 + green3 + green4;
+			}
+			else if (userGuess == "e") {
+				green2 = "e";
+				green3 = "e";
+				greenWord = green0 + green1 + green2 + green3 + green4;
+			}
+			else if (userGuess == "n") {
+				green4 = "n";
+				greenWord = green0 + green1 + green2 + green3 + green4;
+			}
+			else {
+				guesses--;
+				letterCache();
+				if (guesses < 0) {
+					losses++;
+					if (greenIndex > -1) {
+						colorsArray.splice(greenIndex, 1);
+					} 
+					chooseWord();
+				}
+			}
+			if (green0 == "g" && green1 == "r" && green2 == "e" && green3 == "e" && green4 == "n") {
+				wins++;
+				if (greenIndex > -1) {
+						colorsArray.splice(greenIndex, 1);
+				} 
+				chooseWord();
+			}
+			console.log(greenWord);
+			console.log(guesses);
+		}
+
+		var html = "<p>Press any letter to begin game</p>" + 
+		"<p>Wins: " + wins + "</p>" + 
+		"<p>Losses: " + losses + "</p>" + 
+		"<p> Current Word: " + "</p>" + 
+		"<p>" + displayWord + "</p>" + 
+		"<p>Guesses left: " + guesses + "</p>" + 
+		"<p>Letters Used: </p>" + 
+		letterPrint;
+
+		document.querySelector("#game").innerHTML = html;
+
 	}
 
